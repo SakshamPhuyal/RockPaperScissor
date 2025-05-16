6696b0ee0f30d0900aaa933ad1ed400e878d642b
SakshamPhuyal <phuyalsaksham1@gmail.com>
2025-05-16 21:11:11 +0545

    added functions

---
+++ script.js
@@ -0,0 +1,50 @@
+let userScore=0;
+let computerScore=0;
+const choices=document.querySelectorAll(".choice");
+const msg= document.querySelector("#msg");
+const userScorep=document.querySelector("#user-score");
+const computerScorep=document.querySelector("#comp-score");
+const generateChoice=()=>{
+const options=["rock","paper","scissors"];
+const randIdx=Math.floor(Math.random()*3);
+return options[randIdx];
+};
+const showWinner=(userwin)=>{
+    if(userwin){
+        userScore++;
+        userScorep.innerText=userScore;
+        msg.innerText="You win!";
+        msg.style.backgroundColor="green";
+    }else{
+       computerScore++;
+       computerScorep.innerText=computerScore;    
+        msg.innerText="You lose."
+            msg.style.backgroundColor="red";
+    }
+}
+const playGame=(userchoice)=>{
+    const compchoice=generateChoice();
+    if(userchoice === compchoice){
+msg.innerText="Game draw."
+    msg.style.backgroundColor="black";
+    }
+    else{
+        let userwin = true;
+        if(userchoice==="rock"){
+        userwin= compchoice==="paper"?false:true;
+    }
+    else if(userchoice === "paper"){
+userwin=compchoice === "scissors"?false:true;
+}
+else{
+    userwin=compchoice==="rock"?false:true;
+}
+showWinner(userwin);
+    }
+};
+choices.forEach((choice)=>{
+choice.addEventListener("click",()=>{
+    const userchoice=choice.getAttribute("id");
+   playGame(userchoice);
+})
+});
